b33bbb969a509dd02df99f27353a76c5727897e5
Øyvind Nygard <oyvind.nygard@knowit.no>
2019-06-26 12:50:13 +0200

    Added flag for ignoring sensordata (#20)
    * Added flag for ignoring sensordata
    
    * Added queryparam for specifying data source
    
    * Use more elegant solution for filter_source method
    
    * Use kong-url as base path in openapi doc

--- src/main/temperature_reader.py
+++ src/main/temperature_reader.py
@@ -1,25 +1,57 @@
 import boto3
 import json
+import logging
 
 from boto3.dynamodb.conditions import Key
 from copy import deepcopy
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
 sensor_data_table = dynamodb.Table('badetemperatur-latest')
 
 
+data_sources_default = 'manual'
+valid_data_sources = ['manual', 'sensor']
+
+
 def get_all_temperatures(event, context):
-    if event['queryStringParameters']:
-        key = event['queryStringParameters']['location']
-        temperature_item_list = query_for_item(key)
+    logger.info(event)
+    query_params = event['queryStringParameters'] if event['queryStringParameters'] else {}
+
+    location = query_params.get('location')
+
+    data_sources = query_params.get('sources', data_sources_default).split(',')
+
+    if not all(source in valid_data_sources for source in data_sources):
+        return lambda_proxy_response(
+            400,
+            {'message': f'Invalid value, source parameter can only contain values: {valid_data_sources}'}
+        )
+
+    if location:
+        temperature_item_list = query_for_item(location)
     else:
         temperature_item_list = scan_for_items()
 
     response_body = list(
-        map(lambda item: from_dynamodb_format(item), temperature_item_list)
+        filter(lambda item: filter_source(item, data_sources),
+               map(lambda item: from_dynamodb_format(item), temperature_item_list))
+
     )
 
     return lambda_proxy_response(200, response_body)
+
+
+def filter_source(item, sources):
+    if sources:
+        if item['name'] == "Manuell måling":
+            return 'manual' in sources
+        else:
+            return 'sensor' in sources
+    return False
 
 
 def query_for_item(key):
--- src/test/temperature_reader_test.py
+++ src/test/temperature_reader_test.py
@@ -6,32 +6,53 @@ import src.test.test_data.temperature_reader_test_data as test_data
 from src.test.test_utils import create_table
 from moto import mock_dynamodb2
 
-class Tester(unittest.TestCase):
+dynamo_db_items = [
+            test_data.item_1_sensor,
+            test_data.item_1_manual,
+            test_data.item_2_sensor,
+            test_data.item_2_manual
+        ]
 
+class Tester(unittest.TestCase):
     @mock_dynamodb2
     def test_get_all_temperatures(self):
-        table_name = 'badetemperatur-latest'
-        create_table(table_name, [
-            test_data.item_1_sensor,
-            test_data.item_1_manual,
-            test_data.item_2_sensor,
-            test_data.item_2_manual
-        ])
 
-        response = temperature_reader.get_all_temperatures(test_data.http_event_no_query_params, None)
+        table_name = 'badetemperatur-latest'
+        create_table(table_name, dynamo_db_items)
+
+        response = temperature_reader.get_all_temperatures(test_data.http_event_no_location, None)
         response_body = json.loads(response['body'])
-        self.assertCountEqual(response_body, test_data.get_all_temperatures_response_body)
+        self.assertCountEqual(response_body, test_data.get_all_temperatures_manual_only_body)
+
+
+    @mock_dynamodb2
+    def test_get_all_temperatures_with_source_filter(self):
+        table_name = 'badetemperatur-latest'
+        create_table(table_name, dynamo_db_items)
+
+        response_all_sources = temperature_reader.get_all_temperatures(test_data.http_event_no_location_all_sources, None)
+        response_sensor_only = temperature_reader.get_all_temperatures(test_data.http_event_no_location_sensor_only, None)
+        response_manual_only = temperature_reader.get_all_temperatures(test_data.http_event_no_location_manual_only, None)
+        response_invalid_sources = temperature_reader.get_all_temperatures(test_data.http_event_no_location_invalid_sources, None)
+
+        response_all_sources_body = json.loads(response_all_sources['body'])
+        response_sensor_only_body = json.loads(response_sensor_only['body'])
+        response_manual_only_body = json.loads(response_manual_only['body'])
+
+        self.assertCountEqual(response_all_sources_body, test_data.get_all_temperatures_response_body)
+        self.assertCountEqual(response_sensor_only_body, test_data.get_all_temperatures_sensor_only_response_body)
+        self.assertCountEqual(response_manual_only_body, test_data.get_all_temperatures_manual_only_body)
+
+        #self.assertEqual(response_invalid_sources['statusCode'], 400)
+
+
     @mock_dynamodb2
     def test_get_temperature(self):
+        temperature_reader.data_sources_default = 'manual,sensor'
         table_name = 'badetemperatur-latest'
-        create_table(table_name, [
-            test_data.item_1_sensor,
-            test_data.item_1_manual,
-            test_data.item_2_sensor,
-            test_data.item_2_manual
-        ])
+        create_table(table_name, dynamo_db_items)
 
-        response = temperature_reader.get_all_temperatures(test_data.http_event_with_query_params, None)
+        response = temperature_reader.get_all_temperatures(test_data.http_event_with_location, None)
         response_body = json.loads(response['body'])
         self.assertCountEqual(response_body, test_data.get_temperature_response_body)
 
@@ -39,12 +60,7 @@ class Tester(unittest.TestCase):
     @mock_dynamodb2
     def test_query_for_item(self):
         table_name = 'badetemperatur-latest'
-        create_table(table_name, [
-            test_data.item_1_sensor,
-            test_data.item_1_manual,
-            test_data.item_2_sensor,
-            test_data.item_2_manual
-        ])
+        create_table(table_name, dynamo_db_items)
 
         item_list = temperature_reader.query_for_item(test_data.location_id_1)
         self.assertCountEqual(
@@ -55,12 +71,7 @@ class Tester(unittest.TestCase):
     @mock_dynamodb2
     def test_scan_for_items(self):
         table_name = 'badetemperatur-latest'
-        create_table(table_name, [
-            test_data.item_1_sensor,
-            test_data.item_1_manual,
-            test_data.item_2_sensor,
-            test_data.item_2_manual
-        ])
+        create_table(table_name, dynamo_db_items)
 
         items_in_table = temperature_reader.scan_for_items()
         self.assertCountEqual(
@@ -69,6 +80,18 @@ class Tester(unittest.TestCase):
              test_data.item_1_manual,
              test_data.item_2_sensor,
              test_data.item_2_manual])
+
+
+    def test_filter_source(self):
+
+        assert not temperature_reader.filter_source(test_data.item_1_sensor, [])
+        assert not temperature_reader.filter_source(test_data.item_1_manual, ['kake'])
+        assert temperature_reader.filter_source(test_data.item_1_sensor, ['sensor', 'manual'])
+        assert temperature_reader.filter_source(test_data.item_1_manual, ['sensor', 'manual'])
+        assert temperature_reader.filter_source(test_data.item_1_sensor, ['sensor'])
+        assert not temperature_reader.filter_source(test_data.item_1_manual, ['sensor'])
+        assert not temperature_reader.filter_source(test_data.item_1_sensor, ['manual'])
+        assert temperature_reader.filter_source(test_data.item_1_manual, ['manual'])
 
 
     def test_decimal_to_float(self):
